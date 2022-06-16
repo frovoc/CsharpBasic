a5704f0f8e1c18c7f6a5cd61a98fa3a80a722605
frovoc <23167-frovoc@users.noreply.gitgud.io>
2022-06-16 18:02:29 +0800

    1

--- LearnCsharp/WhileForBasic/Program.cs
+++ LearnCsharp/WhileForBasic/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        static int InputNum()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int num;
+                bool success = int.TryParse(input, out num);
+                if (!success)
+                {
+                    Console.WriteLine("请输入格式正确的数字");
+                    continue;
+                }
+                return num;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //while循环
+            //经典写法，分段解决问题
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                int inputnum;
+                bool success = int.TryParse(input, out inputnum);
+                if (!success)
+                {
+                    Console.WriteLine("请输入格式正确的数字");
+                    continue;
+                } 
+                
+                Console.WriteLine(inputnum*2);
+            }
         }
     }
 }
